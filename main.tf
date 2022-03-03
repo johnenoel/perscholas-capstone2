413806a140f0caebfa0381b24738365e7d0233a3
John Noel <johnericknoel@gmail.com>
2022-03-02 20:33:01 -0500

    commit the main tf file

--- main.tf
+++ main.tf
@@ -14,13 +14,34 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_security_group" "secgrp" {
+  name        = "tf-sg"
+  description = "Web Security Group for HTTP"
+
+  ingress {
+    description = "All TCP traffic"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All TCP traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "app_server" {
-  #ami           = "ami-830c94e3"
-  ami           = "ami-08e4e35cccc6189f4"
-  instance_type = "t2.medium"
-  key_name      = "aws_jn_keypair"
+  ami               = "ami-08e4e35cccc6189f4"
+  instance_type     = "t2.medium"
+  key_name          = "aws_jn_keypair"
+  security_groups = ["tf-sg"]
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "k8s-minikube"
   }
 }
